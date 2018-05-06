08ace4e6296d5d925fbf8b7fec3310d286efe1d5
Iryna Skiba <iryna.skiba.98@gmail.com>
2018-05-06 17:46:50 +0300

    add laba 4

---
+++ laba 4/src/my/Fork.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package my;
+
+/**
+ * @author Iryna Skiba
+ *
+ */
+public class Fork {
+	boolean state;
+	/**
+	 * This function show state.
+	 * @return state
+	 */
+	boolean isTaken() {
+		return state;
+	}
+	void setState(boolean state) {
+		this.state = state;
+	}
+	Fork(){
+		state = false;
+	}
+}
---
+++ laba 4/src/my/Main.java
@@ -0,0 +1,12 @@
+package my;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        CreateDisplay createDisplay = new CreateDisplay();
+        createDisplay.CreateWindow();
+    }
+
+}
